a4db750ee6088c519243f47f4a9c99cc8dd8cb1c
yuanhao <t109590041@ntut.org.tw>
2022-03-26 21:38:36 +0800

    拿掉多餘顯示

--- game4.10/Source/mygame.cpp
+++ game4.10/Source/mygame.cpp
@@ -420,19 +420,19 @@ void CGameStateRun::OnShow()
 	//  貼上背景圖、撞擊數、球、擦子、彈跳的球
 	//
 	//background.ShowBitmap();			// 貼上背景圖
-	help.ShowBitmap();					// 貼上說明圖
+	/*help.ShowBitmap();					// 貼上說明圖
 	hits_left.ShowBitmap();
 	for (int i=0; i < NUMBALLS; i++)
 		ball[i].OnShow();				// 貼上第i號球
 	bball.OnShow();						// 貼上彈跳的球
-	eraser.OnShow();					// 貼上擦子
+	eraser.OnShow();					// 貼上擦子*/
 	//
 	//  貼上左上及右下角落的圖
 	//
-	corner.SetTopLeft(0,0);
+	/*corner.SetTopLeft(0,0);
 	corner.ShowBitmap();
 	corner.SetTopLeft(SIZE_X-corner.Width(), SIZE_Y-corner.Height());
-	corner.ShowBitmap();
+	corner.ShowBitmap();*/
 
 	enemy0.OnShow(&gameMap);
 	enemy1.OnShow(&gameMap);
