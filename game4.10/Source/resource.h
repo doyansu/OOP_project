d1b2dc38f24af039dedd36b98aabd6a43c414434
DESKTOP-QQQEDJS\kerueilin9 <kerueilin9>
2022-04-07 23:20:12 +0800

    血量UI

--- game4.10/Source/resource.h
+++ game4.10/Source/resource.h
@@ -162,6 +162,8 @@
 #define IDB_ENEMY2_6_L                  293
 #define IDB_BITMAP69                    294
 #define IDB_ENEMY2_7_L                  294
+#define IDB_BITMAP30                    295
+#define IDB_UI_HP                       295
 #define IDC_README                      1001
 #define ID_FILE_PAUSE                   32771
 #define ID_TOGGLE_FULLSCREEN            32772
@@ -174,7 +176,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        295
+#define _APS_NEXT_RESOURCE_VALUE        296
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
