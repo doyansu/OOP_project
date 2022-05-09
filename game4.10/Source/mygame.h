7e51045aad7526f01ce024a3206f028abb34056f
yuanhao <t109590041@ntut.org.tw>
2022-05-09 15:02:31 +0800

    細節調整

--- game4.10/Source/mygame.h
+++ game4.10/Source/mygame.h
@@ -38,9 +38,6 @@
  *      3. Use ShowInitProgress(percent) to display loading progress.
 */
 
-#include "CEraser.h"
-#include "CBall.h"
-#include "CBouncingBall.h"
 #include "CInitialPlayer.h"
 #include "CGameObj.h"
 #include "CGameMap.h"
